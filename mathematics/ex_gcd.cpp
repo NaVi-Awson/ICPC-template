b1cc1d6c3fb7bb392433edf4f2af0db0829158c8
NaVi-Awson <awsonshen@163.com>
2021-03-24 19:16:33 +0800

    add exgcd

---
+++ mathematics/ex_gcd.cpp
@@ -0,0 +1,17 @@
+template<typename T>
+struct ex_gcd {
+    T gcd(const T a, const T b, T &x, T &y) {// x'=x_0+b/gcd, y'=y_0-a/gcd 
+        if (b == 0) {x = 1, y = 0; return a; }
+        T d = gcd(b, a%b, x, y);
+        T t = x;
+        x = y;
+        y = t - a/b*y;
+        return d;
+    }
+    T inv(const T a, const T m) {// return -1 if inv is not exist
+        if (a == 0 || m <= 1) return -1;
+        T x, y, d = gcd(a, m, x, y);
+        if (d != 1) return -1;
+        return (x%m+m)%m;
+    }
+} ;
