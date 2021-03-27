e67939592e8a29092ea114bf0d7b64e7cf10f210
NaVi-Awson <awsonshen@163.com>
2021-03-27 15:36:51 +0800

    add excrt

---
+++ mathematics/ex_crt.cpp
@@ -0,0 +1,17 @@
+template<typename T>
+struct ex_crt {
+    ex_gcd<T> *exgcd = new ex_gcd<T>();
+    T cal(T *a, T *m, const int n) {// a[1..n], m[1..n], return -1 if no ans
+        T x, y, gcd, lcm;
+        for (int i = 2; i <= n; i++) {
+            gcd = exgcd->gcd(m[1], m[i], x, y);
+            if ((a[i]-a[1])%gcd) return -1;
+            lcm = (__int128)m[1]*m[i]/gcd;
+            x = (__int128)x*(a[i]-a[1])/gcd%lcm;
+            gcd = m[i]/gcd;
+            x = (x%gcd+gcd)%gcd;
+            a[1] = ((__int128)m[1]*x%lcm+a[1])%lcm, m[1] = lcm;
+        }
+        return a[1];
+    }
+} ;
