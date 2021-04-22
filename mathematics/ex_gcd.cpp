282a8a3dc87af49ab93632c2e734590300cd4d95
NaVi-Awson <awsonshen@163.com>
2021-04-22 08:14:34 +0800

    upd exgcd

--- mathematics/ex_gcd.cpp
+++ mathematics/ex_gcd.cpp
@@ -8,7 +8,7 @@ struct ex_gcd {
         y = t - a/b*y;
         return d;
     }
-    T inv(const T a, const T m) {// return -1 if inv is not exist
+    T inv(const T a, const T m) {// return -1 if inv is not exist, require a > 0
         if (a == 0 || m <= 1) return -1;
         T x, y, d = gcd(a, m, x, y);
         if (d != 1) return -1;
